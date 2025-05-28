b43371cd3cbcb94c26661c137137aa4910099c99
muazraja <muazraja@gmail.com>
2025-05-28 15:56:36 +0500

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 export const APP_CONFIG = {
-    backendUrl: "https://owl-cafe-w28n.vercel.app/"
+    backendUrl: "https://owl-cafe-eight.vercel.app/"
     // backendUrl: "http://localhost:5002/"
 }
